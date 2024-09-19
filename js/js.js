358ddf67f77104128b10ee850e17fbc4c5a81caa
osamaabdallah225 <osamaabdallah225@gmail.com>
2024-09-19 16:27:49 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,52 @@
+
+
+   document.getElementById('totalValue').focus(); 
+
+    function calculateTax() {
+        // Get the values from the input fields
+        const totalValue = parseFloat(document.getElementById('totalValue').value);
+        const taxRate = parseFloat(document.getElementById('taxRate').value);
+       
+        
+      
+       // التحقق من صحة الإدخال
+       if (isNaN(totalValue) || totalValue <= 0) {
+                    alert('الرجاء إدخال قيمة إجمالية صحيحة.');
+                        //   البحث نتائج  تصفير
+                      document.getElementById('result-total').value = 0;
+                      document.getElementById('result-tax').value = 0;
+                      document.getElementById('result-totalf').value = 0;
+                      return;
+                  }
+      
+       if (isNaN(taxRate) || taxRate <= 0) {
+                      alert('الرجاء إدخال نسبة ضريبة القيمة المضافة صحيحة.');
+                    //   البحث نتائج  تصفير
+                      document.getElementById('result-total').value = 0;
+                      document.getElementById('result-tax').value = 0;
+                      document.getElementById('result-totalf').value = 0;
+                      return;
+                  }
+        // Calculate the tax amount
+      
+      
+        if (document.getElementById('yes').checked) {
+          
+      
+        const taxAmount = (totalValue * taxRate) / 100;
+       
+        document.getElementById('result-total').value = (totalValue).toLocaleString(3);
+        document.getElementById('result-tax').value = (taxAmount).toLocaleString(3);
+        document.getElementById('result-totalf').value = (totalValue+taxAmount).toLocaleString(3);
+      
+        }
+      
+        else {
+          const taxAmount = (totalValue / (taxRate+totalValue)) * taxRate;
+        document.getElementById('result-total').value = (totalValue-taxAmount).toLocaleString(3);
+        document.getElementById('result-tax').value = (taxAmount).toLocaleString(3);
+        document.getElementById('result-totalf').value = (totalValue).toLocaleString(3);
+        }
+    }
+
+ 
